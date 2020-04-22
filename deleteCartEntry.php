c0664e114cdc97193a1aead00d90189103aebcb7
Banee Ishaque K <baneeishaque@gmail.com>
2020-04-22 20:23:50 +0000

    Standardize

---
+++ deleteCartEntry.php
@@ -0,0 +1,6 @@
+<!--11.1.3.1-->
+<?php
+//TODO : Use POST on deletion/insertion/update tasks
+//TODO : Check for POST Variables
+//$_GET['cartentry'];
+//TODO : Return Success on result ignored deletion tasks
---
+++ getUserCart.php
@@ -0,0 +1,6 @@
+<!--11-->
+<?php
+//TODO : Check for GET Variables
+//$_GET['userid'];
+echo "cart data";
+
