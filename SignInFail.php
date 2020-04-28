251fcaa3c0ab92a7973ac8de8b1d39a55f1334ef
Nabeel Muhammed <mnabeel007@gmail.com>
2020-04-28 18:27:00 +0530

    11

--- SignInFail.php
+++ SignInFail.php
@@ -4,12 +4,7 @@ if (empty($_POST['username'])) {
 
 		 echo "username shouldn't be empty";
 } else {
-	 if (empty($_POST['password'])) {
-
-		   $passwordErr= "password is missing";
-			
-		} else {
 		   echo "sign in failed";
-		}
+
 	}	
 ?>
