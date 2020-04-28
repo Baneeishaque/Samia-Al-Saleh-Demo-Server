da7e18c75373084feec4ebfca0b4acb49e4a585d
Nabeel Muhammed <mnabeel007@gmail.com>
2020-04-28 20:05:11 +0530

    111

--- AddDeliveryBoy.php
+++ AddDeliveryBoy.php
@@ -2,18 +2,15 @@
 if (empty($_POST['name'])) {
 
     echo "name is required";
+} else if (empty($_POST['mobile number'])) {
+    echo "mobile number is required";
+
 } else {
-    if (empty($_POST['mobile number'])) {
-        echo "mobile number is required";
+    $validnum = filter_var($_POST['mobile'], FILTER_SANITIZE_NUMBER_INT);
+    $validnum = str_replace("-", "", $validnum);
+    if (strlen($validnum) < 10 || strlen($validnum) > 14) {
+        echo "invalid mobile number";
 
-    } else {
-        $validnum = filter_var($_POST['mobile'], FILTER_SANITIZE_NUMBER_INT);
-        $validnum = str_replace("-", "", $validnum);
-        if (strlen($validnum) < 10 || strlen($validnum) > 14) {
-            echo "invalid mobile number";
-
-        } else {
-        }
-        echo "Delivery Boy Added Successfully ";
-    }
+    }else
+    echo "Delivery Boy Added Successfully ";
 }
